ca58531f4c1486417fb5434e2a560efe86a4811d
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-12-16 13:19:29 -0500

    Locomotion
    Can be toggled to follow where the player is looking or a general "Forward" direction

--- kkinney_ULTB18_Nov19/Assets/Scripts/Locomotion.cs
+++ kkinney_ULTB18_Nov19/Assets/Scripts/Locomotion.cs
@@ -1,48 +1,119 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
+using Valve.VR;
 
-namespace Valve.VR.InteractionSystem
+
+public class Locomotion : MonoBehaviour
 {
-    [RequireComponent(typeof(Rigidbody))]
-    public class Locomotion : MonoBehaviour
+    public float m_Gravity = 30.0f;
+    public float m_Sensitivity = 0.1f;
+    public float m_MaxSpeed = 1.0f;
+
+    public SteamVR_Action_Boolean m_MovePress = null;
+    public SteamVR_Action_Vector2 m_MoveValue = null;
+
+    private float m_Speed = 0.0f;
+
+    private CharacterController m_CharacterController = null;
+    private Transform m_CameraRig = null;
+    private Transform m_Head = null;
+
+    private void Awake()
     {
-        Hand hand_L;
-        Hand hand_R;
+        m_CharacterController.GetComponent<CharacterController>();
+    }
 
-        Rigidbody rb;
 
-        [Range(1, 100)]
-        public int speed = 10;
+    private void Start()
+    {
+        m_CameraRig = SteamVR_Render.Top().origin;
+        m_Head = SteamVR_Render.Top().head;
+    }
 
-        //public ISteamVR_Action_Vector2 = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("Movement");
 
-        private void Awake()
-        {
-            hand_L = GameObject.Find("LeftHand").GetComponent<Hand>();
-            hand_R = GameObject.Find("RightHand").GetComponent<Hand>();
+    private void Update()
+    {
+        //HandleHead();
+        HandleHeight();
+        CalculateMovement();
+    }
 
-            rb = GetComponent<Rigidbody>();
-        }
+    #region HandleHead Unused
+    /*
+    private void HandleHead()
+    {
+        // Store current
+        Vector3 oldPosition = m_CameraRig.position;
+        Quaternion oldRotation = m_CameraRig.rotation;
 
-        // Start is called before the first frame update
-        void Start()
-        {
+        // Rotation
+        transform.eulerAngles = new Vector3(0, m_Head.rotation.eulerAngles.y, 0);
 
-        }
+        // Restore
+        m_CameraRig.position = oldPosition;
+        m_CameraRig.rotation = oldRotation;
+    }
+    */
+    #endregion
 
-        // Update is called once per frame
-        void Update()
-        {
-            float horizontalAxis = Input.GetAxis("Joystick_X");
-            float verticalAxis = Input.GetAxis("Joystick_Y");
+    private void CalculateMovement()
+    {
+        // Figure out movement orientation
+        Quaternion orientation = CalculateOrientation();
+        Vector3 movement = Vector3.zero;
 
-            Vector3 movement = new Vector3(horizontalAxis, verticalAxis, 0f);
+        // If not moving
+        //if (m_MovePress.GetStateUp(SteamVR_Input_Sources.Any)) // Using any of the specified input from either controller
+        if (m_MoveValue.axis.magnitude == 0)
+            m_Speed = 0;
 
-            Vector3 localMovement = transform.InverseTransformVector(movement);
+        // Add, Clamp
+        //m_Speed = m_MoveValue.axis.y * m_Sensitivity; // Input with direction
+        m_Speed = m_MoveValue.axis.magnitude * m_Sensitivity; // Input is x distance
+        m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed); // Use multiplier on -m_MaxSpeed to decrease walking backwards speed
 
-            rb.AddForce(localMovement * speed);
-        }
+        // Orientation and Gravity
+        movement += orientation * (m_Speed * Vector3.forward);
+        movement.y -= m_Gravity * Time.deltaTime;
+
+        // Apply
+        m_CharacterController.Move(movement * Time.deltaTime);
+    }
+
+    private void HandleHeight()
+    {
+        // Get the head in local space
+        float headHeight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
+        m_CharacterController.height = headHeight;
+
+        // Cut in half
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = m_CharacterController.height / 2f;
+        newCenter.y += m_CharacterController.skinWidth;
+
+        #region Stops Movement into Walls
+        // Move capusle in local space
+        newCenter.x = m_Head.localPosition.x;
+        newCenter.z = m_Head.localPosition.z;
+
+        // Rotate
+        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
+        #endregion
+
+        // Apply
+        m_CharacterController.center = newCenter;
+    }
+
+    private Quaternion CalculateOrientation()
+    {
+        float rotation = Mathf.Atan2(m_MoveValue.axis.x, m_MoveValue.axis.y);
+        rotation *= Mathf.Rad2Deg;
+
+        Vector3 orientationEuler = new Vector3(0, m_Head.eulerAngles.y + rotation, 0);
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+
+        return orientation;
     }
 }
+
